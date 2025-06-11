73acd4950c124aeb0b6c733e2dee930415ab4446
helenzhou6 <25727036+helenzhou6@users.noreply.github.com>
2025-06-11 15:41:17 +0100

    Convert into one main script to download wiki data

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from hackernews-upvote-predictor!")
+from hackernewsupvotepredictor.download_cbow_rawdata import download_wiki_data
 
+def main():
+    url = 'https://huggingface.co/datasets/ardMLX/text8/resolve/main/text8'
+    wiki_data = download_wiki_data(url)
 
 if __name__ == "__main__":
     main()
