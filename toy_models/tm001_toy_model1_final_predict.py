ac4e190ba29919b1ed2b850e2951c3bee0c0a83f
helenzhou6 <25727036+helenzhou6@users.noreply.github.com>
2025-06-11 12:09:59 +0100

    Update final predict with new formula that is more accurate

--- toy_models/tm001_toy_model1_final_predict.py
+++ toy_models/tm001_toy_model1_final_predict.py
@@ -1,101 +1,32 @@
-
-############################################################
-
-### import packages
-
-import pandas as pd
+from scipy.optimize import fsolve
 import numpy as np
 
-# visualization
-import matplotlib.pyplot as plt
+# # Fit a degree-3 polynomial in log(x)
+# t = np.log(x_true)
+# coeffs = np.polyfit(t, y_true, 3)
 
-############################################################
+# # Fit a degree-3 polynomial in log(x)
+# t = np.log(x_true)
+# coeffs = np.polyfit(t, y_true, 3)
 
-### set variable parameters
+coeffs = np.array([0.07538848, -0.76435324,  3.6821385 , -0.24469317])
 
-# focus on score up to 500
-thres = 500
+# Define the polynomial function
+def poly_logx(x, y_target):
+    logx = np.log(x)
+    y = np.polyval(coeffs, logx)
+    return y - y_target  # We want this to be 0
 
-###
-###
-###
+# Solve for x given y, element-wise
+x_recovered = []
+for y_val in y_true:
+    # Provide an initial guess (you can tweak this)
+    x_guess = 1.0
+    x_sol = fsolve(poly_logx, x0=x_guess, args=(y_val,))
+    x_recovered.append(x_sol[0])
 
-############################################################
-
-### load dataset
-
-# load 4mil hacker_news dataset
-# df = pd.read_parquet("hf://datasets/julien040/hacker-news-posts/story.parquet")
-df = pd.read_parquet("../data/submissions.parquet.gzip")
-
-df_non0 = df[df["score"]>0]["score"] # as there are negative scores
-total_count = df_non0.shape[0]
-max_count = df_non0.max()
-min_count = df_non0.min()
-
-print(f"sample size is {total_count}"); 
-print(f"min_count is {min_count}"); 
-print(f"max_count is {max_count}"); 
-
-############################################################
-
-### create a list of tuples (score, a proportion of score over 4 mil)
-
-prop_data = []
-
-for i in range(1,max_count+1):
-    count_prob = df_non0[df_non0==i].shape[0]/total_count
-    if count_prob > 0:
-        # print(f"score-{i}: proportion is {count_prob}")
-        prop_data.append((i, count_prob))
-
-count_np = np.array([count for count, _ in prop_data])
-prob_np = np.array([prob for _, prob in prop_data])
-
-############################################################
-
-### visualization of raw data
-
-plt.plot(count_np, 1/prob_np); plt.savefig("../data/data.png"); plt.show(); 
-# plt.plot(count_np[:thres], 1/prob_np[:thres]); plt.show(); 
-# plt.plot(count_np[:thres], np.log(1/prob_np[:thres])); plt.show(); 
-# plt.plot(count_np[thres:], 1/prob_np[thres:]); plt.show(); 
-# plt.plot(count_np[thres:], np.log(1/prob_np[thres:])); plt.show(); 
-# plt.plot(count_np[thres:], prob_np[thres:]); plt.show(); 
-
-############################################################
-
-### compute a toy formula to fit to data
-
-# actual data with normalization
-x_true = count_np[:thres]
-y_true = np.log(1/prob_np[:thres])
-
-# fitting curve
-
-# Fit a polynomial of degree 1 (a linear function) to the data
-t = np.log(x_true)
-p = np.polyfit(t, y_true, 1)
-
-# Construct the fitted curve
-a = p[0] # it was around 1.6
-b = p[1] # it was around 0.5
-x_fitted = np.linspace(np.min(x_true), np.max(x_true), 100)
-y_fitted = a * np.log(x_fitted) + b
-# print(f"a {a}, b {b}")
-
-# based on the above formula, predict x-val based on actual y-val
-x_recovered = np.round(np.exp( (y_true - b)/a ),0)
-
-# plt.plot(x_true, y_true); plt.plot(x_fitted, y_fitted); plt.show(); 
+x_recovered = np.round(x_recovered, 0)
 
 # compute loss
-# mean squared error (mse)
 mse = np.mean((x_true - x_recovered)**2)
-print(f"mse is {mse}")
-
-############################################################
-
-# next step is 
-# to use neural(linear) model to make more accurate performance
-
+print(mse)
