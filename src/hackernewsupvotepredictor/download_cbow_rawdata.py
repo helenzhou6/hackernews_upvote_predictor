a4962471bc437a39f16b4a5e445d03c8cc5e1278
helenzhou6 <25727036+helenzhou6@users.noreply.github.com>
2025-06-12 11:40:44 +0100

    Clean up CBOW (not need some params) and update main to start to download titles

--- src/hackernewsupvotepredictor/download_cbow_rawdata.py
+++ src/hackernewsupvotepredictor/download_cbow_rawdata.py
@@ -8,7 +8,7 @@ text = data.decode('utf-8')
 with open('data/text8', 'w') as file:
     file.write(text)
 
-def download_wiki_data(url):
+def download_data_from_url(url):
     response = urllib.request.urlopen(url)
     data = response.read()      # a bytes object
     text = data.decode('utf-8')
